c0106adf8cb81b9f0c947fdce07a76413ffbe04d
root <root@ip-172-31-92-232.ec2.internal>
2025-07-03 18:13:20 +0000

    This Code for Terraform Workspaces

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+variable "group" {
+description = "Specify the group (prod or dev)"
+type = string
+default = "dev"
+}
+
+variable "instance_types" {
+type = map(string)
+default = {
+dev = "t2.micro"
+prod = "t2.micro"
+}
+}
+
+resource "aws_instance" "on_demand_instance" {
+ami = "ami-000ec6c25978d5999"
+instance_type = lookup(var.instance_types, var.group, "t2.micro")
+count = var.group == "dev" || var.group == "prod" ? 1:0
+tags = {
+Name = "Instance-for-$(var.group)"
+Group = var.group
+}
+}
